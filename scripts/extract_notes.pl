8c3d8d359372c3b05c614dfb6a82434a6cc03e8d
Brett Deaton <brett.deaton@gmail.com>
2022-12-04 15:01:29 -0800

    Clean up extract_notes.pl
    * print title
    * extract notes including references from 00.md

--- scripts/extract_notes.pl
+++ scripts/extract_notes.pl
@@ -5,12 +5,13 @@ use strict;
 use warnings;
 use File::Basename;
 
+print "# Brett's Notes from React Fundamentals\n";
 foreach (@ARGV) {
     open my $fh, $_ or die "Can't open $_: $!";
     print "\n## ", basename($_), "\n";
     while (<$fh>)
     {
-        if (/^##.*Brett's Notes/i ... /^## /) {
+        if (/^##.*Brett's/i ... /^## /) {
             print $_ if not /^## /;
         }
     }
